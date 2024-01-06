14ef3f2c6e733f890afde5eb56bda5914b4ea39c
Zuniga Lab <zunigalab@Zunigas-MBP.attlocal.net>
2024-01-05 17:28:23 -0800

    Language defaults to english, added show cards to images, covers are now only retrieved if they exist

--- mainScript.js
+++ mainScript.js
@@ -10,26 +10,29 @@ async function searchFunction(){
     const searchInput=document.querySelector('#search').value;
     let newURL=searchInput;
     newURL=newURL.replaceAll(" ", "+");
-    newURL=baseUrl+newURL;
+    newURL=baseUrl+newURL+"+language%3Aeng&limit=3&mode=everything";
     console.log(newURL);
     const response= await fetch(newURL);
     const bookList= await response.json();
     
  for (let index = 0; index < 3; index++) {
-       bookOlid=bookList.docs[index].edition_key[0];
-       coverURL=baseCoverUrl+bookOlid+"-M.jpg"
-       console.log(coverURL);
-       const newCoversContainer=document.querySelector('#imageContainer');
-       let newCoverContainer=document.createElement('div');
-       newCoverContainer.id=`returned cover #:${index}`;
-       let newCover=document.createElement('img');
-       newCover.id=`newCover${index}`
-       let addWorkButton=document.createElement('button');
-       addWorkButton.textContent='Add Me!'
-       newCover.src=coverURL;
-       newCoversContainer.appendChild(newCoverContainer);
-       newCoverContainer.appendChild(newCover);
-       newCoverContainer.appendChild(addWorkButton);
+       bookOlid=bookList.docs[index].cover_edition_key;
+       console.log(bookList.docs[index].cover_edition_key);
+       if (bookOlid=bookList.docs[index].cover_edition_key) {
+        coverURL=baseCoverUrl+bookOlid+"-M.jpg"
+        console.log(coverURL);
+        const newCoversContainer=document.querySelector('#imageContainer');
+        let newCoverContainer=document.createElement('div');
+        newCoverContainer.id=`returned cover #:${index}`;
+        let newCover=document.createElement('img');
+        newCover.id=`newCover${index}`
+        let addWorkButton=document.createElement('button');
+        addWorkButton.textContent='Add Me!'
+        newCover.src=coverURL;
+        newCoversContainer.appendChild(newCoverContainer);
+        newCoverContainer.appendChild(newCover);
+        newCoverContainer.appendChild(addWorkButton);
+       }
  }
 }
 /* This gets a json of a book from open library, then takes the first first 
